c32693d6c187fbcb7758bf93cf70f3780c1d1ed8
CampfireCollective <122759859+GuilloryCraft@users.noreply.github.com>
2025-04-08 12:13:25 -0600

    New deck number 3!

--- src/essay.lua
+++ src/essay.lua
@@ -64,7 +64,7 @@ SMODS.current_mod.extra_tabs = function() --Credits tab
                     {
                     n = G.UIT.T,
                     config = {
-                        text = "bishopcorrigan, tuzzo, R3venantR3mnant, Neato, Sacto",
+                        text = "bishopcorrigan, tuzzo, R3venantR3mnant, Neato, Sacto, Gote",
                         shadow = false,
                         scale = scale,
                         colour = G.C.MONEY
@@ -2978,47 +2978,45 @@ SMODS.Back{ --Archeology Deck
   end
 }
 
--- no art yet lol but feel free to uncomment and play with them
+SMODS.Back{ --Echo Deck
+    name = "Echo Deck",
+	key = "echodeck",  
+  loc_txt = {      
+    name = 'Echo Deck',      
+    text = {
+      "{C:attention}Retrigger{} all playing cards",
+      "{C:red}X2{} base Blind size"
+    } 
+  }, 
+    order = 18,
+  unlocked = true,
+  discovered = true,
+	config = {},
+  loc_vars = function(self, info_queue, center)
+    return {vars = {}}
+  end,
+	pos = { x = 2, y = 0 },
+	atlas = "ECother",
+  apply = function(self, back)
+    G.GAME.starting_params.ante_scaling = 2
+  end,
 
--- SMODS.Back{ --Echo Deck
---     name = "Echo Deck",
--- 	key = "echodeck",  
---   loc_txt = {      
---     name = 'Echo Deck',      
---     text = {
---       "{C:attention}Retrigger{} all playing cards",
---       "{C:red}X2{} base Blind size"
---     } 
---   }, 
---     order = 18,
---   unlocked = true,
---   discovered = true,
--- 	config = {},
---   loc_vars = function(self, info_queue, center)
---     return {vars = {}}
---   end,
--- 	pos = { x = 3, y = 0 },
--- 	atlas = "ECjokers",
---   apply = function(self, back)
---     G.GAME.starting_params.ante_scaling = 2
---   end,
+  calculate = function(self, back, context)
+    if context.cardarea == G.play and context.repetition then
+        return {
+            message = localize('k_again_ex'),
+            repetitions = 1,
+            card = card
+        }
 
---   calculate = function(self, back, context)
---     if context.cardarea == G.play and context.repetition then
---         return {
---             message = localize('k_again_ex'),
---             repetitions = 1,
---             card = card
---         }
-
---     elseif context.repetition and context.cardarea == G.hand then
---         if (next(context.card_effects[1]) or #context.card_effects > 1) then
---             return {
---                 message = localize('k_again_ex'),
---                 repetitions = 1,
---                 card = card
---             }
---         end
---     end
---   end
--- }
+    elseif context.repetition and context.cardarea == G.hand then
+        if (next(context.card_effects[1]) or #context.card_effects > 1) then
+            return {
+                message = localize('k_again_ex'),
+                repetitions = 1,
+                card = card
+            }
+        end
+    end
+  end
+}
